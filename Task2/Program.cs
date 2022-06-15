e1788e6b29efc3901b29d5b4a379a82e567a0a0c
SergeiEremkin <ffr@list.ru>
2022-06-15 23:22:35 +0300

    add 3 tasks from seminar3(methods)

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿void Distance3D (int Ax, int Ay, int Az, int Bx, int By, int Bz )
+{
+    double AB = Math.Sqrt(Math.Pow((Bx-Ax), 2)+Math.Pow((By-Ay), 2)+Math.Pow((Bz-Az), 2));
+    System.Console.WriteLine(AB);
+}
+Distance3D(2,3,4,5,6,7);
+Distance3D(2,-3,4,5,-6,7);
+Distance3D(2,23,4,5,57,7);
+
---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿void CubeNumber(int N)
+{
+    for(double i = 1; i<=N; i++)
+    {
+        double cube = Math.Pow(i,3);
+        System.Console.Write($"{cube}  ");
+    }
+}
+CubeNumber(3);
+System.Console.WriteLine();
+CubeNumber(25);
+System.Console.WriteLine();
+CubeNumber(10);
